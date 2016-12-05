5a2196d1d98e07b7fe435c017964acd54aca5d41
Mayank4545 <keshri.mayank@gmail.com>
2016-12-06 00:37:45 +0530

    Assignment3.1
    Correct answer :
    c. It will compile and run printing out "10“

---
+++ Assignment3_1.java
@@ -0,0 +1,18 @@
+
+public class Assignment3_1 {
+	static{
+		print(10);
+	}
+	static void print (int x) {
+		System.out.println (x);
+		System.exit(0);
+	}
+}
+
+/* Solution */
+
+//c. It will compile and run printing out "10"
+/* Program will call static and print value of X i.e. "10"
+ * and then it will go to search for main method and stop
+ * once it won't find any. 
+ * Thus, correct answer is : c. It will compile and run printing out "10" */
